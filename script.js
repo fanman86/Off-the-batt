938d5cc1844b72c0143f89aa182160bd193a5741
fanman86 <likumbivusumuzhi@gmail.com>
2026-05-20 08:24:48 +0200

    20/05/2026

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ function greetUser(){
     let User=
 document.getElementById("username").value;
 document.getElementById("message").textContent
-="Hi there,"+User+"want to know about artizell? click the blue button below!!!";
+="Hi there,"+User+""+"want to know about artizell? click the blue button below!!!";
 }
