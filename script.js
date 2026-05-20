6912e4fc1ddcf74e7cac7e5a942744ef557668f0
fanman86 <likumbivusumuzhi@gmail.com>
2026-05-20 07:37:05 +0200

    20/05/2026

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function greatUser(){
     let User=
 document.getElementById=("nameinput").value;
-document.getElementById=("message").contenttext
-"Hi there"+User;
+document.getElementById=("message").textContent
+="Hi there"+User;
 }
